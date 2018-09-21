fc4c7f62702ad96e17dcd017bd6f1a8653b3dac1
Christopher Siu <christopher@siu.sh>
2018-09-21 09:36:02 -0700

    i think the autograder is broken

--- planets.py
+++ planets.py
@@ -1,7 +1,10 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
-if __name__ == '__main__':
+    weight = int(input("What do you weigh on earth? "))
+
+    print("\nOn Mars you would weigh %.2f pounds."
+          "\nOn Jupiter you would weigh %.2f pounds."
+          % (weight * 0.38, weight * 2.34))
+
+
+if __name__ == "__main__":
    weight_on_planets()
